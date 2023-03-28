b766d0e4e5b7d3bd4a5c6e85ac96f78f8793b4a2
Aaron-KASONGO <66473473+Aaron-KASONGO@users.noreply.github.com>
2023-03-28 02:10:05 +0000

    Rightbar component

--- dashboard-social/src/components/RightBar.js
+++ dashboard-social/src/components/RightBar.js
@@ -1,4 +1,4 @@
-import { Box, ImageList, ImageListItem, Typography } from "@mui/material"
+import { Avatar, AvatarGroup, Box, Divider, ImageList, ImageListItem, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material"
 import { itemData } from "./ImgData"
 
 
@@ -14,21 +14,102 @@ export const RightBar = () => {
         sm: 'block'
     }}
     >
-      <Typography variant="h6" fontWeight={100}>Online Friends</Typography>
-      <Typography variant="h6" fontWeight={100}>Latest Photos</Typography>
-      <ImageList cols={3} rowHeight={130}>
-        {
-          itemData.map((item) => (
-            <ImageListItem key={item.img}>
-              <img
-              src={`${item.img}??w=164&h=164&fit=crop&auto=format&dpr=2`}
-              alt={item.title}
-              loading="lazy"
-              />
-            </ImageListItem>
-          ))
-        }
-      </ImageList>
+      <Box position="fixed" width={300}>
+        <Typography variant="h6">Online Friends</Typography>
+        <AvatarGroup max={8}>
+          <Avatar alt="Remy Sharp" src="https://randomuser.me/api/portraits/men/20.jpg" />
+          <Avatar alt="Travis Howard" src="https://randomuser.me/api/portraits/men/7.jpg" />
+          <Avatar alt="Cindy Baker">MK</Avatar>
+          <Avatar alt="Agnes Walker" src="https://randomuser.me/api/portraits/men/14.jpg" />
+          <Avatar alt="Trevor Henderson" src="https://randomuser.me/api/portraits/women/20.jpg" />
+          <Avatar alt="Cindy Baker">KD</Avatar>
+          <Avatar alt="Trevor Henderson" src="https://randomuser.me/api/portraits/women/24.jpg" />
+          <Avatar alt="Trevor Henderson" src="https://randomuser.me/api/portraits/men/20.jpg" />
+          <Avatar alt="Trevor Henderson" src="https://randomuser.me/api/portraits/men/20.jpg" />
+        </AvatarGroup>
+        <Typography variant="h6">Latest Photos</Typography>
+        <ImageList cols={3} rowHeight={100}>
+          {
+            itemData.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                src={`${item.img}?w=164&h=100&fit=crop&auto=format&dpr=2`}
+                alt={item.title}
+                loading="lazy"
+                />
+              </ImageListItem>
+            ))
+          }
+        </ImageList>
+        <Typography variant="h6">Latest Convesation</Typography>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="https://randomuser.me/api/portraits/men/7.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="https://randomuser.me/api/portraits/women/99.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="https://randomuser.me/api/portraits/men/14.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </>
+              }
+            />
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   )
 }

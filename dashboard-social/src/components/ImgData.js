e34257f4fa3caf6f801016f51d8d1d4b483ed792
Aaron-KASONGO <66473473+Aaron-KASONGO@users.noreply.github.com>
2023-03-28 01:27:13 +0000

    adding profil and Latest photos

--- dashboard-social/src/components/ImgData.js
+++ dashboard-social/src/components/ImgData.js
@@ -56,3 +56,18 @@ export const imgData = [
 		img: "https://images.unsplash.com/photo-1677668802610-1a3eb78f1560"
 	}
 ]
+
+export const itemData = [
+	{
+		title: "Abat jour",
+		img: "https://images.unsplash.com/photo-1679926820630-bb34ce597056"
+	},
+	{
+		title: "Abat jour",
+		img: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4"
+	},
+	{
+		title: "Abat jour",
+		img: "https://images.unsplash.com/photo-1414235077428-338989a2e8c0"
+	}
+]
--- dashboard-social/src/components/RightBar.js
+++ dashboard-social/src/components/RightBar.js
@@ -1,11 +1,11 @@
-import { Box } from "@mui/material"
+import { Box, ImageList, ImageListItem, Typography } from "@mui/material"
+import { itemData } from "./ImgData"
 
 
 export const RightBar = () => {
   return (
     <Box
     sx={{
-        bgcolor: 'error.light',
         flex: 2,
         p: 2
     }}
@@ -13,6 +13,22 @@ export const RightBar = () => {
         xs: 'none',
         sm: 'block'
     }}
-    >SideBar</Box>
+    >
+      <Typography variant="h6" fontWeight={100}>Online Friends</Typography>
+      <Typography variant="h6" fontWeight={100}>Latest Photos</Typography>
+      <ImageList cols={3} rowHeight={130}>
+        {
+          itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+              src={`${item.img}??w=164&h=164&fit=crop&auto=format&dpr=2`}
+              alt={item.title}
+              loading="lazy"
+              />
+            </ImageListItem>
+          ))
+        }
+      </ImageList>
+    </Box>
   )
 }
